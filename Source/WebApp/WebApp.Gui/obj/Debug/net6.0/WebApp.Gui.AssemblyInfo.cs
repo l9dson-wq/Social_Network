9d3a5cb51f03950e7fac939488a3d35d21469431
l9dson-wq <andelsonnicolas@gmail.com>
2023-12-06 21:35:09 -0800

    TailwindCSS added
    TailwindCSS added using npm and its modules.

--- Source/WebApp/WebApp.Gui/obj/Debug/net6.0/WebApp.Gui.AssemblyInfo.cs
+++ Source/WebApp/WebApp.Gui/obj/Debug/net6.0/WebApp.Gui.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp.Gui")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6848cc22a6011ae87dcd6b508d237e0db22467ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp.Gui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp.Gui")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
