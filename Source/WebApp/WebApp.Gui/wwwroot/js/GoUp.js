77314f1b698e2f511d87ce9c3e2162a16362c0e6
Andelson <ledson@Andelsons-iMac-Pro.local>
2024-01-02 19:49:23 -0400

    Back to Top button added
    When user scrolls down a button will appears and the user will be able to get back top the top of the website.

--- Source/WebApp/WebApp.Gui/wwwroot/js/GoUp.js
+++ Source/WebApp/WebApp.Gui/wwwroot/js/GoUp.js
@@ -1,3 +1,21 @@
-function scrollToTop() {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
+// GO UP FUNCTION --------------------------
+window.onscroll = function() {
+  mostrarOcultarBoton();
+};
+
+function mostrarOcultarBoton() {
+  var boton = document.getElementById("volver-arriba");
+
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    boton.style.display = "block";
+  } else {
+    boton.style.display = "none";
+  }
 }
+
+function volverArriba() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+//--------------------------
