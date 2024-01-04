a5951c9f91200a92eb86bc972f4f9a2e6a26aea1
Andelson <ledson@Andelsons-iMac-Pro.local>
2024-01-03 20:52:48 -0400

    Adding new design to the profile view
    Adding new design to the profile view

---
+++ Source/WebApp/WebApp.Gui/Controllers/ProfileController.cs
@@ -0,0 +1,43 @@
+using Core.Application;
+using Core.Application.ViewModels.Home;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Gui.Middlewares;
+
+namespace WebApp.Gui.Controllers;
+
+public class ProfileController : Controller
+{
+  private readonly ValidateUserSession _validateUserSession;
+  private readonly IPostService _iPostService;
+  private readonly IUserProfileService _iUserProfileService;
+  private readonly IHttpContextAccessor _iHttpContextAccessor;
+  private readonly UserProfileViewModel _userProfileViewModel;
+  
+  public ProfileController(
+    ValidateUserSession validateUserSession, 
+    IPostService iPostService, 
+    IUserProfileService iUserProfileService,
+    IHttpContextAccessor iHttpContextAccessor)
+  {
+    _validateUserSession = validateUserSession;
+    _iPostService = iPostService;
+    _iUserProfileService = iUserProfileService;
+    _iHttpContextAccessor = iHttpContextAccessor;
+    _userProfileViewModel = _iHttpContextAccessor.HttpContext.Session.Get<UserProfileViewModel>("userProfile");
+  }
+  
+  // GET
+  public async Task<IActionResult> Index()
+  {
+    if (!_validateUserSession.HasUser())
+      return RedirectToRoute(new {controller = "Login", action = "Index"});
+    
+    var homeViewModel = new HomeViewModel
+    {
+      UserProfileViewModel = _userProfileViewModel,
+      PostViewModels = await _iPostService.GetAllViewModelWithInclude()
+    };
+      
+    return View(homeViewModel);
+  }
+}
---
+++ Source/WebApp/WebApp.Gui/wwwroot/js/ShowPostInformation.js
@@ -0,0 +1,6 @@
+// get document elements
+
+
+function getPostInformation(){
+  
+}
