3df01abd27176821e55259b970f385cf8f3a4c93
Andelson <ledson@Andelsons-iMac-Pro.local>
2024-01-07 22:04:13 -0400

    Comments in posts updated
    Comments in posts updated

--- Source/Core/Core.Application/Services/CommentService.cs
+++ Source/Core/Core.Application/Services/CommentService.cs
@@ -25,32 +25,14 @@ public class CommentService : CommonService<SaveCommentViewModel, CommentViewMod
       Id = comment.Id,
       Description = comment.Description,
       ImagePath = comment.ImagePath,
-      userId = comment.UserId,
+      UserId = comment.UserId,
       PostId = comment.PostId ?? 0,  // Si comment.PostId es null, se asigna 0 como valor predeterminado
       User = _iMapper.Map<UserViewModel>(comment.User),
       Post = _iMapper.Map<PostViewModel>(comment.Post),
       Likes = _iMapper.Map<List<LikeViewModel>>(comment.Likes),
       Replies = _iMapper.Map<List<CommentViewModel>>(comment.Replies),
       ParentComment = _iMapper.Map<CommentViewModel>(comment.ParentComment),
-    }).ToList();
-  }
-  
-  public async Task<List<CommentViewModel>> CustomGetAllViewModelWithInclude()
-  {
-    var comments = await _iCommentRepository.GetAllWithIncludeAsync(new List<string>{"Likes","Replies","ParentComment"});
-
-    return comments.Select(comment => new CommentViewModel()
-    {
-      Id = comment.Id,
-      Description = comment.Description,
-      ImagePath = comment.ImagePath,
-      userId = comment.UserId,
-      PostId = comment.PostId ?? 0,
-      User = _iMapper.Map<UserViewModel>(comment.User),
-      Post = _iMapper.Map<PostViewModel>(comment.Post),
-      Likes = _iMapper.Map<List<LikeViewModel>>(comment.Likes),
-      Replies = _iMapper.Map<List<CommentViewModel>>(comment.Replies),
-      ParentComment = _iMapper.Map<CommentViewModel>(comment.ParentComment),
+      ParentCommentId = comment.ParentCommentId,
     }).ToList();
   }
 }
--- Source/Core/Core.Application/ViewModels/Comments/CommentViewModel.cs
+++ Source/Core/Core.Application/ViewModels/Comments/CommentViewModel.cs
@@ -8,13 +8,12 @@ public class CommentViewModel
   public int Id { get; set; }
   public string Description { get; set; }
   public string ImagePath { get; set; }
-  public int userId { get; set; }
+  public int UserId { get; set; }
   public int? ParentCommentId { get; set; }
   public UserViewModel User { get; set; }
   public PostViewModel Post { get; set; }
   public CommentViewModel ParentComment { get; set; }
   public ICollection<LikeViewModel> Likes { get; set; }
   public ICollection<CommentViewModel> Replies { get; set; }
-  public int UserId { get; set; }
   public int PostId { get; set; }
 }
